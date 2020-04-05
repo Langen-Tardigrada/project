c062d5ea9b2c6d9b1af37feb01cb51b7d62c5f61
Langen-Tardigrada <46591485+Langen-Tardigrada@users.noreply.github.com>
2020-04-05 20:00:04 +0700

    ...2

--- booking/src/component/Home.jsx
+++ booking/src/component/Home.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import '../bookStyle.css'
+//import { render } from 'react-dom'
+import Person from 'react-ionicons/lib/MdPerson'
+import Arrow from 'react-ionicons/lib/IosArrowDropright'
+import Cash from 'react-ionicons/lib/MdCash'
+
 
 class Home extends React.Component{
     render(){
@@ -10,37 +15,52 @@ class Home extends React.Component{
                        . h o t e l  
                     </div>
                     <div>
-                        <form action="" > 
-                            <div>
-                                <label className="label"> Type Room : </label>
-                                <select className="typeRoom">
-                                    <option value="Delux">Delux room</option>
-                                    <option value="Sweet">Sweet room</option>
-                                    <option value="Studio">Studio room</option>
-                                    <option value="Common">Common room</option>
-                                </select>
-                            </div>
-                            <div>
-                                <label className="label"> Check in : </label>
-                                <input className="input" type="date"  name="checkin" value="2020-04-04" min="2020-04-05" max="2021-12-31"/>
-                            </div>
-                            <div>
-                                <label className="label"> Check out : </label>
-                                <input className="input" type="date" name="checkout" value="2020-04-05" min="2020-04-05" max="2021-12-31"/>
-                            </div>
-                            <div>
-                                <input className="input" type="submit" value="Find"/>
-                            </div>
+                        <form action="" className="form" > 
+                            <label className="label"> Type Room : </label>
+                            <select className="typeRoom">
+                                <option value="Superior">Superior room</option>
+                                <option value="Delux">Delux room </option>
+                                <option value="Sweet">Seeet room</option>
+                                <option value="Suit">Suit room</option>
+                                <option value="Family">Family room</option>
+                            </select>
+                            <label className="label"> Check in : </label>
+                            <input type="date"  name="checkin" value="2020-04-04" min="2020-04-05" max="2021-12-31"/>
+                            <label className="label"> Check out : </label>
+                            <input type="date" name="checkout" value="2020-04-05" min="2020-04-05" max="2021-12-31"/>
+                            <input type="submit" value="FIND"/>   
                         </form>
                     </div>
                 </div>
-                <div className = "result">
-                    <div>
+                <div className = "result-list">
+                    <div className = "result">
                         Result list
                     </div>
-                    <div className = "List">
-                        
-                    </div>
+                    <table className ="list">
+                        <tr className = "row">
+                            <td className = "typeroom">
+                                SWEET ROOM
+                            </td>
+                            <td className ="number">
+                                S201
+                            </td>
+                            <td className = "iconPerson">
+                                <Person fontSize="27px" color="#31312E"/>
+                            </td >
+                            <td className = "amount">
+                                2
+                            </td>
+                            <td className = "iconCash">
+                                <Cash fontSize="30px" color="#31312E"/>
+                            </td>
+                            <td className = "course">
+                             3000
+                            </td>
+                            <td className = "iconArrow">
+                                <Arrow fontSize="30px" color="#31312E"/>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </section>
             
