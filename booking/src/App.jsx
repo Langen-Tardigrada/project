d768568961a2dfd9b81503ba9b17ca9715ab9396
IAMPATTRIXX <dexza53@gmail.com>
2020-04-07 01:38:25 +0700

    test

--- booking/src/App.jsx
+++ booking/src/App.jsx
@@ -2,56 +2,79 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 import Home from './component/Home'
-// import firebase from './firebase/firebase'
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import firebase from './firebase/firebase'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default class App extends Component {
   //**********For Add Firebase************/
-  // state = {
-  //   test: []
-  // }
+  state = {
+    test: []
+  }
 
-  // validateForm = (name) => {
-  //   if (!name) {
+  validateForm = (name) => {
+    if (!name) {
+      window.alert('Please fill in ALL data fields.');
+      return false;
+    } else if ( !isNaN(name)) {
+      window.alert('Please fill only TEXT detail in transaction name.');
+      return false;
+    }
+      return true;
+  }
+
+  addData = (name) => {
+    
+    if(!this.validateForm(name)) {
+      return false;
+    }
+
+    const newData = {
+      name: name,  
+    }
+    
+    firebase.firestore().collection('test').add(newData)
+    this.state.test.unshift(newData);
+    this.setState( { test: this.state.test } );
+  }
+
+  find = () => {
+    return true
+  } 
+
+  // validate = () => {
+  //   if (!name || !amount) {
   //     window.alert('Please fill in ALL data fields.');
   //     return false;
   //   } else if ( !isNaN(name)) {
   //     window.alert('Please fill only TEXT detail in transaction name.');
   //     return false;
-  //   }
-  //     return true;
-  // }
-
-  // addData = (name) => {
-    
-  //   if(!this.validateForm(name)) {
+  //   } else if (+amount === 0) {
+  //     window.alert('Amount CANNOT be zero!');
   //     return false;
   //   }
-
-  //   const newData = {
-  //     name: name,  
+  //   else if(!Number.isInteger(Number(amount))){
+  //     window.alert('Amount CANNOT be float !')
+  //     return false
   //   }
-    
-  //   firebase.firestore().collection('test').add(newData)
-  //   this.state.test.unshift(newData);
-  //   this.setState( { test: this.state.test } );
+  
+  //   return true;
   // }
 
   render() {
     return (
-      <Home/>
-      // <Router>
-      // <div className="container mt-4 mb-5">
+      // <Home/>
+      <Router>
+      <div className="container mt-4 mb-5">
 
-      //   <Route exact path="/" render={ props => (
-      //     <div>
-      //     <Home addData={this.addData} />
+        <Route exact path="/" render={ props => (
+          <div>
+          <Home addData={this.addData} />
           
-      //     </div>
-      //   )} />
+          </div>
+        )} />
                  
-      // </div>
-      // </Router>
+      </div>
+      </Router>
     )
   }
 
--- booking/src/component/Home.jsx
+++ booking/src/component/Home.jsx
@@ -8,75 +8,39 @@ import Cash from 'react-ionicons/lib/MdCash'
 
 class Home extends React.Component{
     
-    // state = {
-    //     name:'',
-    //   }
+    state = {
+        name:'',
+      }
     // อีเว้นท์เมื่อคลิก เพื่อใช้ในการอัพ firebase
-    //   onChange = (e) => {
-    //     this.setState( { [e.target.name]: e.target.value } );
-    //   }
+      onChange = (e) => {
+        this.setState( { [e.target.name]: e.target.value } );
+      }
     
-    //   onSubmit = (e) => {
-    //     e.preventDefault();
-    //     this.props.addData(this.state.name);
-    //     this.setState( { name:''});
-            
+      onSubmit = (e) => {
+        e.preventDefault();
+        this.props.addData(this.state.name);
+        this.setState( { name:''});
+      }   
     render(){
         return(      
-            <section className = "section container" >
-                <div className = "center" >
-                    <div className = "nameHotel">
-                       . h o t e l  
-                    </div>
-                    <div>
-                        <form action="" className="form" > 
-                            <label className="label"> Type Room : </label>
-                            <select className="typeRoom">
-                                <option value="Superior">Superior room</option>
-                                <option value="Delux">Delux room </option>
-                                <option value="Sweet">Seeet room</option>
-                                <option value="Suit">Suit room</option>
-                                <option value="Family">Family room</option>
-                            </select>
-                            <label className="label"> Check in : </label>
-                            <input type="date"  name="checkin" value="2020-04-04" min="2020-04-05" max="2021-12-31"/>
-                            <label className="label"> Check out : </label>
-                            <input type="date" name="checkout" value="2020-04-05" min="2020-04-05" max="2021-12-31"/>
-                            <input type="submit" value="FIND"/>   
-                        </form>
-                    </div>
-                </div>
-                <div className = "result-list">
-                    <div className = "result">
-                        Result list
-                    </div>
-                    <table className ="list">
-                        <tr className = "row">
-                            <td className = "typeroom">
-                                SWEET ROOM
-                            </td>
-                            <td className ="number">
-                                S201
-                            </td>
-                            <td className = "iconPerson">
-                                <Person fontSize="27px" color="#31312E"/>
-                            </td >
-                            <td className = "amount">
-                                2
-                            </td>
-                            <td className = "iconCash">
-                                <Cash fontSize="30px" color="#31312E"/>
-                            </td>
-                            <td className = "course">
-                             3000
-                            </td>
-                            <td className = "iconArrow">
-                                <Arrow fontSize="30px" color="#31312E"/>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </section>
+    <div>
+        <form id="add-transaction_form" onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Transaction Name:</label>
+            <input 
+              type="text" 
+              name="name" 
+              className="form-control" 
+              placeholder="Expense detail ..."
+              onChange={this.onChange}
+              value={this.state.name}
+            />
+          </div>
+          
+
+          <input type="submit" value="Add Transaction" className="btn btn-primary btn-block"/>
+        </form>
+    </div>
             
         );
     } 
